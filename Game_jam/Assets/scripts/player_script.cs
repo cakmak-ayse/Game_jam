e83920321c84444c975fe472606b3948b981afd1
cakmak-ayse <62162794+cakmak-ayse@users.noreply.github.com>
2024-06-05 17:49:47 -0400

    player movement
    jalskdj

--- Game_jam/Assets/scripts/player_script.cs
+++ Game_jam/Assets/scripts/player_script.cs
@@ -5,28 +5,72 @@ using UnityEngine;
 public class player_script : MonoBehaviour
 {
     
-    public Rigidbody2D myRigidBody;
-    public float jumoStrength = 5;      // to change flap play with this and gravity scale on rigid-body2d
-    // Start is called before the first frame update
-    void Start()
-    {
+    // public Rigidbody2D myRigidBody;
+    // public float jumoStrength = 5;      // to change flap play with this and gravity scale on rigid-body2d
+    // // Start is called before the first frame update
+    // void Start()
+    // {
         
-    }
+    // }
 
-    // Update is called once per frame
-    void Update()
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     //jump ( make sure it can jump only once later on lol now its a flappy bird)
+    //     if(Input.GetKeyDown(KeyCode.Space) == true){
+    //         myRigidBody.velocity = Vector2.up * jumoStrength;
+    //     }
+    //     // go right
+    //     if(Input.GetKey(KeyCode.RightArrow) == true){
+    //         myRigidBody.velocity = Vector2.right;
+    //     }
+    //     // go left
+    //     if(Input.GetKey(KeyCode.LeftArrow) == true){
+    //         myRigidBody.velocity = Vector2.left;
+    //     }
+    // }
+
+    //Movement
+    public float speed;
+    public float jump;
+    float moveVelocity;
+
+    //Grounded Vars
+    bool grounded = true;
+
+    void Update () 
     {
-        //jump ( make sure it can jump only once later on lol now its a flappy bird)
-        if(Input.GetKeyDown(KeyCode.Space) == true){
-            myRigidBody.velocity = Vector2.up * jumoStrength;
+        //Jumping
+        if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Z) || Input.GetKeyDown (KeyCode.W)) 
+        {
+            if(grounded)
+            {
+                GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jump);
+            }
         }
-        // go right
-        if(Input.GetKeyDown(KeyCode.RightArrow) == true){
-            myRigidBody.velocity = Vector2.right;
+
+        moveVelocity = 0;
+
+        //Left Right Movement
+        if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) 
+        {
+            moveVelocity = -speed;
         }
-        // go left
-        if(Input.GetKeyDown(KeyCode.LeftArrow) == true){
-            myRigidBody.velocity = Vector2.left;
+        if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) 
+        {
+            moveVelocity = speed;
         }
+
+        GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
+
+    }
+    //Check if Grounded
+    void OnTriggerEnter2D()
+    {
+        grounded = true;
+    }
+    void OnTriggerExit2D()
+    {
+        grounded = false;
     }
 }
