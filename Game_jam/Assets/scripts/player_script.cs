d073752817f9aab8e835e972d6596b863d32fb85
cakmak-ayse <62162794+cakmak-ayse@users.noreply.github.com>
2024-06-17 01:23:57 -0400

    fucking around and finding out

--- Game_jam/Assets/scripts/player_script.cs
+++ Game_jam/Assets/scripts/player_script.cs
@@ -32,83 +32,51 @@ public class player_script : MonoBehaviour
 
     //Movement
 
-    private Animator animate;
-    //public float moveSpeed = 6f;
-    public float jump = 8f;
-    bool isLookingRight = true;
-    private Rigidbody2D rb;
-    private bool grounded = true;
-    public float speed;
-    float moveVelocity;
-
-    Vector2 movement;
-
-    private void Start()
-    {
-        animate = gameObject.GetComponent<Animator>();
-        rb = gameObject.GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        animate.SetFloat("Speed", Mathf.Abs(movement.x));
-    }
-
-    void FixedUpdate()
-    {
-        moveVelocity = 0;
-        //Flipping and left and right movement
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) && !(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)))
-        {
-            if(isLookingRight){
-                gameObject.GetComponent<SpriteRenderer>().flipX = !gameObject.GetComponent<SpriteRenderer>().flipX;
-                isLookingRight = false;
-            }
-            moveVelocity = -speed;
-        } 
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) && !(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)))
-        {
-            if(!isLookingRight){
-                gameObject.GetComponent<SpriteRenderer>().flipX = !gameObject.GetComponent<SpriteRenderer>().flipX;
-                isLookingRight = true;
-            }
-            moveVelocity = speed;
-        }
-
-        //Jumping
-        if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Z) || Input.GetKeyDown (KeyCode.W)) 
-        {
-            if(grounded)
-            {
-                GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jump);
-            }
-        }
-
-        rb.velocity = new Vector2 (moveVelocity, rb.velocity.y);
-    }
-
-    //Check if Grounded
-    void OnCollisionEnter2D()
-    {
-        grounded = true;
-    }
-    void OnCollisionExit2D()
-    {
-        grounded = false;
-    }
-
-    // Betul movement code
+    // private Animator animate;
+    // //public float moveSpeed = 6f;
+    // public float jump = 8f;
+    // bool isLookingRight = true;
+    // private Rigidbody2D rb;
+    // private bool grounded = true;
     // public float speed;
-    // public float jump;
     // float moveVelocity;
 
-    // //Grounded Vars
-    // bool grounded = true;
+    // Vector2 movement;
 
-    // void Update () 
+    // private void Start()
     // {
+    //     animate = gameObject.GetComponent<Animator>();
+    //     rb = gameObject.GetComponent<Rigidbody2D>();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     movement.x = Input.GetAxisRaw("Horizontal");
+    //     animate.SetFloat("Speed", Mathf.Abs(movement.x));
+    // }
+
+    // void FixedUpdate()
+    // {
+    //     moveVelocity = 0;
+    //     //Flipping and left and right movement
+    //     if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) && !(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)))
+    //     {
+    //         if(isLookingRight){
+    //             gameObject.GetComponent<SpriteRenderer>().flipX = !gameObject.GetComponent<SpriteRenderer>().flipX;
+    //             isLookingRight = false;
+    //         }
+    //         moveVelocity = -speed;
+    //     } 
+    //     if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) && !(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)))
+    //     {
+    //         if(!isLookingRight){
+    //             gameObject.GetComponent<SpriteRenderer>().flipX = !gameObject.GetComponent<SpriteRenderer>().flipX;
+    //             isLookingRight = true;
+    //         }
+    //         moveVelocity = speed;
+    //     }
+
     //     //Jumping
     //     if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Z) || Input.GetKeyDown (KeyCode.W)) 
     //     {
@@ -118,23 +86,10 @@ public class player_script : MonoBehaviour
     //         }
     //     }
 
-    //     moveVelocity = 0;
-
-    //     //Left Right Movement
-    //     if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) 
-    //     {
-    //         moveVelocity = -speed;
-    //     }
-    //     if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) 
-    //     {
-    //         moveVelocity = speed;
-    //     }
-
-    //     GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
-
+    //     rb.velocity = new Vector2 (moveVelocity, rb.velocity.y);
     // }
-    
-    //Check if Grounded
+
+    // //Check if Grounded
     // void OnCollisionEnter2D()
     // {
     //     grounded = true;
@@ -143,4 +98,49 @@ public class player_script : MonoBehaviour
     // {
     //     grounded = false;
     // }
+
+    //Betul movement code
+    public float speed;
+    public float jump;
+    float moveVelocity;
+
+    //Grounded Vars
+    bool grounded = true;
+
+    void Update () 
+    {
+        //Jumping
+        if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Z) || Input.GetKeyDown (KeyCode.W)) 
+        {
+            if(grounded)
+            {
+                GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jump);
+            }
+        }
+
+        moveVelocity = 0;
+
+        //Left Right Movement
+        if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) 
+        {
+            moveVelocity = -speed;
+        }
+        if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) 
+        {
+            moveVelocity = speed;
+        }
+
+        GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
+
+    }
+    
+    //Check if Grounded
+    void OnCollisionEnter2D()
+    {
+        grounded = true;
+    }
+    void OnCollisionExit2D()
+    {
+        grounded = false;
+    }
 }
